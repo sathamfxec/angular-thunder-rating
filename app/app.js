fd2f335e7405519fa3698d8917f4c62033ec2d7e
Satham Hussain <33371243+sathamfxec@users.noreply.github.com>
2017-11-05 23:26:04 +0530

    Created: app js file.
    Created: app js file.

---
+++ app/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+* App initialization.
+*/
+
+angular.module('ThunderRating', [
+    "app.controllers", "app.directive"
+]);
+
+angular.module("app.controllers", []);
+angular.module("app.directive", []);
